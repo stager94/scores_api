e25d40600031e55372e025baa97cfac784393d87
Stanislav Gernyak <stager94@mail.ru>
2017-01-11 15:03:15 +0200

    0

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :scm, :git
 set :repo_url, 'git@github.com:stager94/scores_api.git'
 
 set :keep_releases, 5
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/unicorn.rb}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 
