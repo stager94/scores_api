50a1cdbca1d29c20b0242e8eb913919b0b26ce03
Stanislav Gernyak <stager94@mail.ru>
2017-01-11 14:53:59 +0200

    disable sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-require 'sidekiq/web'
-require 'sidetiq/web'
+# require 'sidekiq/web'
+# require 'sidetiq/web'
 
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  mount Sidekiq::Web => '/jobs'
+  # mount Sidekiq::Web => '/jobs'
 
   namespace :api do
     namespace :v1 do
