bdd30601e2f0f1228957295149c693a7d7d9d2d8
Stanislav Gernyak <stager94@mail.ru>
2017-01-13 13:36:12 +0200

    -

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,9 +1,14 @@
 class TeamSerializer < ActiveModel::Serializer
   attributes :title_en, :title_ru, :title_de,
              :full_title_en, :full_title_ru, :full_title_de,
-             :logo_url, :id
+             :logo_url, :id, :sofascore_url
 
   def logo_url
     URI.join(ActionController::Base.asset_host, object.logo.url).to_s
   end
+
+  def sofascore_url
+    "test"
+  end
+
 end
