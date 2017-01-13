48d11fef573c31a83ac06c732d4ddbb68e1d39e5
Stanislav Gernyak <stager94@mail.ru>
2017-01-13 13:12:30 +0200

    fix team serializer

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,7 +1,7 @@
 class TeamSerializer < ActiveModel::Serializer
   attributes :title_en, :title_ru, :title_de,
              :full_title_en, :full_title_ru, :full_title_de,
-             :logo_url
+             :logo_url, :id
 
   def logo_url
     URI.join(ActionController::Base.asset_host, object.logo.url).to_s
