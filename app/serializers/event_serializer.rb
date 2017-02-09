5c3801121d426e628015c8b88b6b701192bb554d
Stanislav Gernyak <stager94@mail.ru>
2017-02-09 08:41:16 +0200

    add first goal to events API

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,11 +1,16 @@
 class EventSerializer < ActiveModel::Serializer
 
-  attributes :id, :name, :protocol, :home_score, :away_score, :home_scores, :away_scores
+  attributes :id, :name, :protocol, :home_score, :away_score, :home_scores, :away_scores, :first_goal
 
   belongs_to :home_team
   belongs_to :away_team
   belongs_to :venue
   belongs_to :referee
   belongs_to :status
+
+  def first_goal
+    return if incidents.where(incident_type: "goal").order(time: :asc).count == 0
+    incidents.where(incident_type: "goal").order(time: :asc)[0].is_home? ? "home" : "away"
+  end
 
 end
