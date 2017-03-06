fe563fb822a162d25c754d42092ada325becf462
Stanislav Gernyak <stager94@mail.ru>
2017-03-06 21:46:28 +0200

    fix

--- app/workers/sync/competitions/events.rb
+++ app/workers/sync/competitions/events.rb
@@ -7,7 +7,8 @@ module Sync
       def perform(competition_id)
         competition = Competition.find competition_id
         SofaScore::Football::Seasons::Synchronizer.new(competition_id).execute unless competition.seasons.active
-        ::SofaScore::Football::Competitions::Events.new(competition.seasons.active.id).execute
+        active_season = competition.reload.seasons.active
+        ::SofaScore::Football::Competitions::Events.new(active_season.id).execute if active_season.present?
       end
 
     end
