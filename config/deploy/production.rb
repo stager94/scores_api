f7e590690be2a88485e932dd21a52b6be55a802c
Stanislav Gernyak <stager94@mail.ru>
2017-03-16 16:00:22 +0200

    fix deploy config (sidekiq)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,3 +11,8 @@ set :deploy_to, '/var/www/scores_api'
 set :branch, 'master'
 set :unicorn_config_path, "#{shared_path}/config/unicorn.rb"
 set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
+
+set :sidekiq_pid, "#{shared_path}/tmp/pids/sidekiq.pid"
+set :sidekiq_log, "#{shared_path}/log/sidekiq.log"
+set :sidekiq_config, "#{shared_path}/config/sidekiq.yml"
+set :sidekiq_concurrency, 5
