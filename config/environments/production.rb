3a382f2149ba7c5d020ee3c59a596d40f59faa7e
Stanislav Gernyak <stager94@mail.ru>
2017-03-25 13:34:29 +0200

    fix asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_controller.asset_host = 'http://5.9.32.194'
+  config.action_controller.asset_host = 'http://82.196.15.197'
 end
